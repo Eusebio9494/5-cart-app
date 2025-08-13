7f40b80762cd2b593a01f29d3eca8b70e59d0e7b
Eusebio Olvera Chávez <eusebio_olvera9494@hotmail.com>
2025-08-13 14:00:19 -0600

    Se instala depenencia  React Routert

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { CartApp } from './CartApp'
+import { BrowserRouter } from 'react-router-dom'
 
-createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <CartApp />
-  // </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <CartApp />
+    </BrowserRouter>
+  </React.StrictMode>
 )
