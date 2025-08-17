90c603c2e27b4cf33fa8dc2e1ecefba7192e4444
Eusebio Olvera Chávez <eusebio_olvera9494@hotmail.com>
2025-08-16 23:37:36 -0600

    Se desacoplan rutas del componente CartApp al nuevo componente CartRoutes

--- src/CartApp.jsx
+++ src/CartApp.jsx
@@ -1,8 +1,9 @@
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import CartView from "./components/CartView"
 import { CatalogView } from "./components/CatalogView.jsx"
 import { useItemCart } from "./hooks/useItemCart.js";
 import { Navbar } from "./components/Navbar.jsx";
+import CartRoutes from "./routes/CartRoutes.jsx";
 
 
 export const CartApp = () => {
@@ -15,20 +16,10 @@ export const CartApp = () => {
             <div className="container my-4">
 
                 <h3>Cart App</h3>
-                <Routes>
-                    <Route
-                        path="/catalog"
-                        element={<CatalogView handler={newItem => (handlerAddProduct(newItem))} />} />
-
-                    <Route
-                        path="/cart"
-                        element={
-                            <div className="my-3 w-50">
-
-                                {items?.length <= 0 ? <p>No hay productos en el carrito</p> : <CartView items={items} handlerDeleteItem={deleteItem => (handlerDeleteProduct(deleteItem))} />}
-                            </div>
-                        } />
-                </Routes>
+                <CartRoutes
+                    items={items}
+                    handlerAddProduct={handlerAddProduct}
+                    handlerDeleteProduct={handlerDeleteProduct} />
 
             </div>
         </>
